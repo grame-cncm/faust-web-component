b6c92c5a4368af83d602054b339fc86e4c67fd2c
Ian Clester <ijc@ijc8.me>
2023-07-31 12:22:49 -0400

    Add basic fausteditor-style syntax highlighting

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,29 @@
 import {EditorView, basicSetup} from "codemirror"
 import {StreamLanguage} from "@codemirror/language"
-import {c} from "@codemirror/legacy-modes/mode/clike"
+import {clike} from "@codemirror/legacy-modes/mode/clike"
 
-let editor = new EditorView({
-  extensions: [basicSetup, StreamLanguage.define(c)],
+const keywords = "process component import library declare with environment route waveform soundfile"
+const atoms = "mem prefix int float rdtable rwtable select2 select3 ffunction fconstant fvariable button checkbox vslider hslider nentry vgroup hgroup tgroup vbargraph hbargraph attach acos asin atan atan2 cos sin tan exp log log10 pow sqrt abs min max fmod remainder floor ceil rint"
+
+function words(str: string) {
+  const obj: {[key: string]: true} = {}
+  const words = str.split(" ")
+  for (let i = 0; i < words.length; i++) obj[words[i]] = true
+  return obj
+}
+
+const faust = clike({
+  name: "clike",
+  multiLineStrings: true,
+  keywords: words(keywords),
+  atoms: words(atoms),
+  hooks: {
+    "@": () => "meta",
+    "'": () => "meta",
+  }
+})
+
+const editor = new EditorView({
+  extensions: [basicSetup, StreamLanguage.define(faust)],
   parent: document.body
 })
