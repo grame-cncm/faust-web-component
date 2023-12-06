0f4b74f58c2135f16552aeb841b37aff8784fae4
Simon-L <lumis@xulepth.fr>
2023-12-06 03:23:48 +0100

    Always build twice to generate both variants, this is a workaround See https://github.com/rollup/rollup/issues/2756 and https://github.com/vitejs/vite/discussions/1736

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,20 @@
 import { resolve } from 'path'
 import { defineConfig } from 'vite'
 
-export default defineConfig({
-    build: {
-        lib: {
-            entry: resolve(__dirname, process.env.FAUST_WEB_BASIC ? "src/main-basic.ts" : "src/main.ts"),
-            name: "faust_web_component",
-            formats: ["iife"],
-            fileName: () => {
-                if (process.env.FAUST_WEB_BASIC) { return "faust-web-component-basic.js" }
-                else { return "faust-web-component.js" }
+export default defineConfig(({ mode }) => {
+    return {
+        build: {
+            lib: {
+                entry: resolve(__dirname, mode == "basic" ? "src/main-basic.ts" : "src/main.ts"),
+                name: "faust_web_component",
+                formats: ["iife"],
+                fileName: () => {
+                    if (mode == "basic") { return "faust-web-component-basic.js" }
+                    else { return "faust-web-component.js" }
+                },
             },
-        },
-        sourcemap: true,
-    },
+            sourcemap: true,
+            emptyOutDir: false,
+        }
+    }
 })
